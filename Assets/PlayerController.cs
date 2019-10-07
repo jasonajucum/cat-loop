6689939dc011188bdb1ea8d4f8c3c2106be78830
Jason Ajucum <jasonajucum@knights.ucf.edu>
2019-10-06 21:41:43 -0400

    Initial commit

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Animator anim;
+
+    // The code above this comment creates an Animator variable called "anim" so that we can do things with it later; the code below it in the Start function sets the value of our anim variable using our Animator component in Unity
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            anim.SetInteger("State", 1);
+        }
+
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            anim.SetInteger("State", 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            anim.SetInteger("State", 2);
+        }
+
+        if (Input.GetKeyUp(KeyCode.R))
+        {
+            anim.SetInteger("State", 0);
+        }
+
+        if (Input.GetKey("escape"))
+        {
+            Application.Quit();
+        }
+    }
+}
